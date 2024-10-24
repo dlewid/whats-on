9f0c9964ffa4f70c2825202bb9c8618cb89de478
josephschildt <schildtjoey@gmail.com>
2024-10-24 14:40:15 -0500

    added in Events that can be added on admin page with start date,time, and type of sport

--- events/admin.py
+++ events/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+# put our models here to import from models
+from .models import Event
+
+admin.site.register(Event)
+
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('title', 'sports_category', 'date')
+    list_filter = ('sports_category', 'date')
+    search_fields = ('title', 'sports_category')
--- events/models.py
+++ events/models.py
@@ -1,4 +1,9 @@
-from django.db import models
 
+from django.db import models
+from django.utils import timezone
 # Create your models here.
 
+class Event(models.Model):
+    title = models.CharField(max_length=200)
+    sport_category = models.CharField(max_length=200)
+    date = models.DateTimeField()
