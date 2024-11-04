a5663988ff1cff04224bc786624fa531937c833d
Daulton Widlacki <soldbuydaulton@gmail.com>
2024-11-04 14:18:52 -0600

    Changed the model to match the db table 'events'

--- events/models.py
+++ events/models.py
@@ -1,11 +1,16 @@
 
 from django.db import models
 from django.utils import timezone
-# Create your models here.
+from django.db import models
 
 class Event(models.Model):
-    sport_category = models.CharField(max_length=200)
-    home_team = models.CharField(max_length=200)
-    away_team = models.CharField(max_length=200)
-    date = models.DateTimeField()
-    time = models.DateTimeField(default=timezone.now)
+    event_id = models.AutoField(primary_key=True)  # This is your primary key
+    home_team = models.CharField(max_length=200, default="none")
+    away_team = models.CharField(max_length=200, default="none")
+    sport = models.CharField(max_length=200, default="none")  # This should match your DB column
+    event_date = models.DateField(default="2020-01-01")  # Date field
+    event_time = models.TimeField(default="00:00")  # Time field
+
+    class Meta:
+        db_table = 'events'  # Ensures this model uses the existing events table in the DB
+        managed = False
