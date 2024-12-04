24fddddfc6e19e9f049b7b5a347419085c35ac1e
josephschildt <schildtjoey@gmail.com>
2024-12-04 16:58:33 -0600

    fixed styling issues

--- myproject/settings.py
+++ myproject/settings.py
@@ -44,7 +44,7 @@ ROOT_URLCONF = 'myproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,7 +120,6 @@ STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
-    BASE_DIR / "static/bootstrap-5.3.3-dist",
 ]
 
 # Default primary key field type
