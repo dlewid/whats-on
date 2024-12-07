d49361b322a3a94c191665086f52c697a4a843ec
Daulton Widlacki <soldbuydaulton@gmail.com>
2024-12-07 11:26:55 -0600

    added the start of the reviews page

--- events/models.py
+++ events/models.py
@@ -3,14 +3,21 @@ from django.db import models
 from django.utils import timezone
 from django.db import models
 
+
 class Event(models.Model):
     event_id = models.AutoField(primary_key=True)  # This is your primary key
     home_team = models.CharField(max_length=200, default="none")
     away_team = models.CharField(max_length=200, default="none")
-    sport = models.CharField(max_length=200, default="none")  # This should match your DB column
+    # This should match your DB column
+    sport = models.CharField(max_length=200, default="none")
     event_date = models.DateField(default="2020-01-01")  # Date field
     event_time = models.TimeField(default="00:00")  # Time field
 
     class Meta:
         db_table = 'events'  # Ensures this model uses the existing events table in the DB
         managed = False
+
+
+class Review(models.Model):  # The Review model
+    review_text = models.CharField(max_length=200)
+    stars = models.IntegerField(default=0)
--- events/views.py
+++ events/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-from .models import Event
+from .models import Event, Review
 
 # Create your views here.
-
+from .forms import ReviewForm
 from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render, redirect
 
 
 def index(request):
@@ -15,4 +16,23 @@ def index(request):
 
 
 def reviews(request):
-    return render(request, 'events/review.html')
+    if request.method == "POST":
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            form.save()
+            print("form was saved")
+            return redirect("events:reviews")
+    else:
+        form = ReviewForm()
+
+    review_list = Review.objects.all()
+    # This is done so the stars variable can be an iterable list
+    reviews_with_stars = [
+        {
+            **review.__dict__,
+            'star_range': range(review.stars),
+            'no_star_range': range(5 - review.stars),
+        }
+        for review in review_list
+    ]
+    return render(request, "events/review.html", {"form": form, "reviews": reviews_with_stars})
